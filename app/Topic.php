4e43963cd6d709321981fb754aa8da0066f20fb7
mf-jz <976060770@qq.com>
2018-12-24 14:38:08 +0800

    11-7周边模型关联

--- app/Topic.php
+++ app/Topic.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Topic extends Model
 {
-    //
+    // 某主题下的全部文章
+    public function post()
+    {
+        return $this->belongsToMany('App/Post', 'post_topic', 'topic_id', 'post_id');
+    }
+
+    // 某主题下的文章数(withCount)
+    public function postTopic()
+    {
+        return $this->hasMany('App/PostTopic', 'topic_id', 'id');
+    }
 }
