d6283407f7b2220d86c3281f8d5ae63655167b4e
unknown <976060770@qq.com>
2019-05-06 15:51:02 +0800

    15-3创建表和模型

---
+++ app/AdminPermission.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+
+class AdminPermission extends Model
+{
+    // 连接数据表
+    protected $table = 'admin_permissions';
+}
---
+++ app/AdminRole.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+
+class AdminRole extends Model
+{
+    // 连接数据表
+    protected $table = 'admin_roles';
+}
