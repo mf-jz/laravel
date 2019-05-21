50cbed4cefcd7cd64a7f356ac2fcb51eb0b09f37
mf-jz <976060770@qq.com>
2019-05-21 17:28:21 +0800

    17-4使用resource完成后台页面和逻辑

--- routes/admin.php
+++ routes/admin.php
@@ -55,10 +55,17 @@ Route::group(['prefix' => 'admin'], function () {
             Route::post('posts/{post}/status', '\App\Admin\Controllers\PostController@status');
         });
 
-        Route::group(['middlware' => 'can:topic'], function () {
+        Route::group(['middleware' => 'can:topic'], function () {
             # 专题管理
             Route::resource('topics', '\App\Admin\Controllers\TopicController', [
                 'only' => ['index', 'create', 'store', 'destroy']
+            ]);
+        });
+
+        Route::group(['middleware' => 'can:notice'], function () {
+            # 通知管理
+            Route::resource('notices', '\App\Admin\Controllers\NoticeController', [
+                'only' => ['index', 'create', 'store']
             ]);
         });
     });
